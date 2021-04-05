98108d84c08eec79f4e0aa262b10e32ddce82e94
eugen0 <leahu.eugen@yahoo.com>
2021-04-05 13:48:09 +0100

    added the elements according to specs

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var year = document.getElementById("years").value;
+
+    // validation principal input
+    if (principal == "" || principal < 1){
+        alert("Please inser a positive value")
+
+        // focus on princpal afer ok pressd from alert
+        document.getElementById("principal").focus();
+    } else  {
+    // converting the string value of interst, years & rate to num;
+    var p_principal = parseFloat(principal);
+    var p_rate = parseFloat(rate);
+    var p_year = parseFloat(year);
     
+    //calculating interest 
+    var interest = p_principal * p_year * p_rate /100;
+
+    // displaying interest reuslt 
+    var result = document.getElementById("result");
+    result.innerHTML = "<strong> " + interest +"</strong>";
+
+    // write message to user;
+    var d = new Date();
+    var current_year = d.getFullYear();
+    var display_year = Number(current_year) + p_year;
+    var total = p_principal + parseFloat(interest);
+    
+    var message =  "<p> If you deposit " + principal + "<br>"
+    + " at an interest rate of " + rate + ". <br>" +
+    "You will receive as interest amount of " +
+     interest + ",<br>" + "in the year " + display_year  + 
+     ".<br>" + "Total amount, interest plus initial principal is " +
+      total;
+    document.getElementById("message").innerHTML = message;
+    }
 }
-        
+  
+//displaying rate value on slider activations
+function rateChange(){
+    var rate = document.getElementById("rate").value;
+    document.getElementById("ratedisplay").innerHTML = rate + " %";
+}
